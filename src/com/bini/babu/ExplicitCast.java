22afde1d958263b8d47c7f57130909813c9c54ec
Bini Babu <bini.babu@dbizsolution.com>
2024-09-27 12:14:10 +0530

    explicit type casting

---
+++ src/com/bini/babu/ExplicitCast.java
@@ -0,0 +1,15 @@
+package com.bini.babu;
+
+public class ExplicitCast {
+
+	public static void main(String[] args) {
+     int i = 100;
+     byte b = (byte)i;
+     System.out.println(b);
+     
+     int s = 97;
+     char ch = (char)s;
+     System.out.println(ch);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module ExplicitCast {
+}
